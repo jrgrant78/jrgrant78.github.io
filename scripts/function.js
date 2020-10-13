f4dcf05ca7f01c9f0f74227d960df499455fda24
jrgrant78 <jrgrant78@yahoo.com>
2020-10-13 10:12:56 -0400

    upd

--- scripts/function.js
+++ scripts/function.js
@@ -1,12 +1,61 @@
 /* Navigation */
 function mobileBarFunction(x) { x.classList.toggle("change");
 	var y = document.getElementById('MmenuHide'); if (y.style.display === 'block') { y.style.display = 'none'; x.classList.remove("visible"); } else { y.style.display = "block"; x.classList.add("visible"); } }
+/*
 function scrollToHome(){ document.getElementById("home").scrollIntoView({ behavior: 'smooth', block: 'start' }); }
 function scrollToStart(){ document.getElementById("start").scrollIntoView({ behavior: 'smooth', block: 'start' }); }
 function scrollToIntro(){ document.getElementById("intro").scrollIntoView({ behavior: 'smooth', block: 'start' }); }
 function scrollToServices(){ document.getElementById("services").scrollIntoView({ behavior: 'smooth', block: 'start' }); }
 function scrollToAbout(){ document.getElementById("about").scrollIntoView({ behavior: 'smooth', block: 'start' }); }
 function scrollToClients(){ document.getElementById("clients").scrollIntoView({ behavior: 'smooth', block: 'start' }); }
+*/
+
+
+function getElementY(query) {
+  return window.pageYOffset + document.querySelector(query).getBoundingClientRect().top
+}
+
+function doScrolling(element, duration) {
+	var startingY = window.pageYOffset
+  var elementY = getElementY(element)
+  // If element is close to page's bottom then window will scroll only to some position above the element.
+  var targetY = document.body.scrollHeight - elementY < window.innerHeight ? document.body.scrollHeight - window.innerHeight : elementY
+	var diff = targetY - startingY
+  // Easing function: easeInOutCubic
+  // From: https://gist.github.com/gre/1650294
+  var easing = function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 }
+  var start
+
+  if (!diff) return
+
+	// Bootstrap our animation - it will get called right before next frame shall be rendered.
+	window.requestAnimationFrame(function step(timestamp) {
+    if (!start) start = timestamp
+    // Elapsed miliseconds since start of scrolling.
+    var time = timestamp - start
+		// Get percent of completion in range [0, 1].
+    var percent = Math.min(time / duration, 1)
+    // Apply the easing.
+    // It can cause bad-looking slow frames in browser performance tool, so be careful.
+    percent = easing(percent)
+
+    window.scrollTo(0, startingY + diff * percent)
+
+		// Proceed with animation as long as we wanted it to.
+    if (time < duration) {
+      window.requestAnimationFrame(step)
+    }
+  })
+}
+
+// Apply event handlers. Example of firing the scrolling mechanism.
+document.getElementById('scrollMid').addEventListener('click', doScrolling.bind(null, '#intro', 1000))
+document.getElementById('scrollTop').addEventListener('click', doScrolling.bind(null, '#home', 1500))
+document.getElementById('scrollBot').addEventListener('click', doScrolling.bind(null, '#clients', 4000))
+
+
+
+
 function navSelector(e) {
 	var elems = document.querySelector(".active");
 	if(elems !==null){ elems.classList.remove("active"); }
