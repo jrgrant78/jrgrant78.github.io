005603e3042e1a54234a8e650dfe4c3a371d0809
jrgrant78 <jrgrant78@yahoo.com>
2020-10-13 11:18:22 -0400

    upd

--- scripts/function.js
+++ scripts/function.js
@@ -1,14 +1,13 @@
 /* Navigation */
 function mobileBarFunction(x) { x.classList.toggle("change");
 	var y = document.getElementById('MmenuHide'); if (y.style.display === 'block') { y.style.display = 'none'; x.classList.remove("visible"); } else { y.style.display = "block"; x.classList.add("visible"); } }
-/*
+
 function scrollToHome(){ document.getElementById("home").scrollIntoView({ behavior: 'smooth', block: 'start' }); }
 function scrollToStart(){ document.getElementById("start").scrollIntoView({ behavior: 'smooth', block: 'start' }); }
 function scrollToIntro(){ document.getElementById("intro").scrollIntoView({ behavior: 'smooth', block: 'start' }); }
 function scrollToServices(){ document.getElementById("services").scrollIntoView({ behavior: 'smooth', block: 'start' }); }
 function scrollToAbout(){ document.getElementById("about").scrollIntoView({ behavior: 'smooth', block: 'start' }); }
 function scrollToClients(){ document.getElementById("clients").scrollIntoView({ behavior: 'smooth', block: 'start' }); }
-*/
 
 function navSelector(e) {
 	var elems = document.querySelector(".active");
