f383d0e7a3bd2bb8d8ae44683e8a2041505b366f
jrgrant78 <jrgrant78@yahoo.com>
2020-10-19 13:14:14 -0400

    upd

--- scripts_spiritwear/function.js
+++ scripts_spiritwear/function.js
@@ -373,7 +373,7 @@ var itemTable = new function () {
                 this.header.push(obj);              // PUSH (ADD) DATA TO THE JSON ARRAY.
                 this.createTable();                 // REFRESH THE TABLE.
 				listNo += 1;
-				orderString.textContent += listNo + ") \n" + "Item: " + itemList.selectedIndex.textContent + " \n" + "Size: " + sizeList.selectedIndex.textContent + " \n" + "Color: " + colorList.selectedIndex.textContent + " \n" + "Design: " + designString.value + " \n" + "Customization: " + customtxt.value + " \n" + "Cost: " + costString.value + " \n";
+				orderString.textContent += listNo + ") \n" + "Item: " + itemList.options[itemList.selectedIndex].value + " \n" + "Size: " + sizeList.options[sizeList.selectedIndex].value + " \n" + "Color: " + colorList.options[colorList.selectedIndex].value + " \n" + "Design: " + designString.value + " \n" + "Customization: " + customtxt.value + " \n" + "Cost: " + costString.value + " \n";
 				orderList.setAttribute('value', orderString.textContent);
 				itemList.selectedIndex = 0;
 				customtxt.value = '';
