475f55b50e27e9aeb987498aa5cdb49865052891
jrgrant78 <jrgrant78@yahoo.com>
2020-11-26 02:11:07 -0500

    upd

--- scripts/function.js
+++ scripts/function.js
@@ -3,10 +3,8 @@ let menuSection = document.querySelectorAll('menu li');
 // for clickable event
 menuSection.forEach(v=> {
 	v.onclick = (()=> {
-		setTimeout(()=> {
 		menuSection.forEach(j=> j.classList.remove('active'))
 		v.classList.add('active')
-		},300)
 	})
 })
 
