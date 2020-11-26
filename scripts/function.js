9f54dfabdbe3eb6db6030382a014614ac3ec31ca
jrgrant78 <jrgrant78@yahoo.com>
2020-11-25 22:13:08 -0500

    upd

--- scripts/function.js
+++ scripts/function.js
@@ -1,38 +1,3 @@
-const makeNavLinksSmooth = ( ) => {
-  const navLinks = document.querySelectorAll( '.nav-link' );
-
-  for ( let n in navLinks ) {
-    if ( navLinks.hasOwnProperty( n ) ) {
-      navLinks[ n ].addEventListener( 'click', e => {
-        e.preventDefault( );
-        document.querySelector( navLinks[ n ].hash )
-          .scrollIntoView( {
-            behavior: "smooth"
-          } );
-      } );
-    }
-  }
-}
-
-const spyScrolling = ( ) => {
-  const sections = document.querySelectorAll( '.hero-bg' );
-
-  window.onscroll = ( ) => {
-    const scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
-
-    for ( let s in sections )
-      if ( sections.hasOwnProperty( s ) && sections[ s ].offsetTop <= scrollPos ) {
-        const id = sections[ s ].id;
-        document.querySelector( '.active' ).classList.remove( 'active' );
-        document.querySelector( `a[href*=${ id }]` ).parentNode.classList.add( 'active' );
-      }
-  }  
-}
-
-makeNavLinksSmooth( );
-spyScrolling( );
-
-
 /* Navigation */
 function mobileBarFunction(x) { x.classList.toggle("change");
 	var y = document.getElementById('menu'); if (y.style.display === 'block') { y.style.display = 'none'; x.classList.remove("visible"); } else { y.style.display = "block"; x.classList.add("visible"); } }
