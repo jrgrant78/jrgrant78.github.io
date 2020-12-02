f5fb7e8445643fa6180bf942a3a6d5eb886020bf
jrgrant78 <jrgrant78@yahoo.com>
2020-12-02 17:26:17 -0500

    upd

--- scripts/function.js
+++ scripts/function.js
@@ -34,7 +34,7 @@ $('wrapper').bind('scroll', function() {
 	elems.each(function(index){
 		var elemTop 	= ($(this).offset().top + $('wrapper').scrollTop() - $('wrapper').offset().top - 1);
 		var elemBottom 	= elemTop + $(this).height();
-		if ((currentTop >= elemTop && currentTop <= elemBottom) || currentTop == $('#home').position()){
+		if ((currentTop >= elemTop && currentTop <= elemBottom) || currentTop == $('#home').position().top){
 			var id 		= $(this).attr('id');
 			var navElem = $('a[href="#' + id+ '"]');
 			navElem.parent().addClass('active').siblings().removeClass('active');
