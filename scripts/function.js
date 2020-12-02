efc82ea8d65898e112f4813762ac011838545e95
jrgrant78 <jrgrant78@yahoo.com>
2020-12-02 12:52:59 -0500

    upd

--- scripts/function.js
+++ scripts/function.js
@@ -30,25 +30,19 @@ window.onscroll = (()=> {
 */
 
 // smooth scrolling (jquery)
-$('.menuTags') {
-	// Remove links that don't actually link to anything
-	.not('[href="#"]')
-	.not('[href="#0"]')
-	.click(function(event) {
-		// On-page links
-		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname){
-			// Figure out element to scroll to
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			// Does a scroll target exist?
-			if (target.length) {
-				// Only prevent default if animation is actually gonna happen
-				event.preventDefault();
-				$('wrapper').stop().animate({ scrollTop: target.offset().top + $('wrapper').scrollTop() - $('wrapper').offset().top }, 500, function() {});
-			}
+$('.menuTags')
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname){
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		if (target.length) {
+			event.preventDefault();
+			$('wrapper').stop().animate({ scrollTop: target.offset().top + $('wrapper').scrollTop() - $('wrapper').offset().top }, 500, function() {});
 		}
-	});
-}
+	}
+});
 
 // scrollspy (jquery)
 $('wrapper').bind('scroll', function() {
