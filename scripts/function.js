fa2cfc23d7d567cb89abc12b79a3b258064ca4a1
jrgrant78 <jrgrant78@yahoo.com>
2021-05-12 17:17:00 -0400

    upd

--- scripts/function.js
+++ scripts/function.js
@@ -47,6 +47,29 @@ $('wrapper').bind('scroll', function() {
 
 /* CONTACT FORM */
 
+/* PHONE NUMBER FORMAT */
+const isNumericInput = (event) => {
+	const key = event.keyCode;
+	return ((key >= 48 && key <= 57) || (key >= 96 && key <= 105)); };
+const isModifierKey = (event) => {
+	const key = event.keyCode;
+	return (event.shiftKey === true || key === 35 || key === 36) || (key === 8 || key === 9 || key === 13 || key === 46) || (key > 36 && key < 41) || ((event.ctrlKey === true || event.metaKey === true) && (key === 65 || key === 67 || key === 86 || key === 88 || key === 90)) };
+const enforceFormat = (event) => {
+	if (!isNumericInput(event) && !isModifierKey(event)) { event.preventDefault(); } };
+const formatToPhone = (event) => {
+	if (isModifierKey(event)) { return; }
+	const target = event.target;
+	const input = event.target.value.replace(/\D/g,'').substring(0,10);
+	const zip = input.substring(0,3);
+	const middle = input.substring(3,6);
+	const last = input.substring(6,10);
+	if (input.length > 6) { target.value = `(${zip}) ${middle} - ${last}`; }
+	else if (input.length > 3) { target.value = `(${zip}) ${middle}`; }
+	else if (input.length > 0) { target.value = `(${zip}`; } };
+const inputElement = document.getElementById('phoneNumber');
+inputElement.addEventListener('keydown',enforceFormat);
+inputElement.addEventListener('keyup',formatToPhone);
+
 function scheduleCheck() {
     if (document.getElementById('scheduleYes').checked) {
         document.getElementById('ifSchedule').style.display = 'block';
